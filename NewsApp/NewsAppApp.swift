de93ac3dbc9d5639b1d07d6b3aecc26a2376d518
Ishaan-Goel <139787489+Ishaan-Goel@users.noreply.github.com>
2024-01-03 14:41:56 -0800

    Initial Commit

---
+++ NewsApp/NewsAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  NewsAppApp.swift
+//  NewsApp
+//
+//  Created by Ishaan Goel on 1/3/24.
+//
+
+import SwiftUI
+
+@main
+struct NewsAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
